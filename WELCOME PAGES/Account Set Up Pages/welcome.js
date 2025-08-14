8383a94bc974dd04e4a220a1bee87789aba79a9a
paschalkachi <paschalnnamani10@gmail.com>
2025-08-14 18:31:32 +0100

    improved background colors for better UI

--- WELCOME PAGES/Account Set Up Pages/welcome.js
+++ WELCOME PAGES/Account Set Up Pages/welcome.js
@@ -1,8 +1,8 @@
 const welcomePage = `
      <div class="bg-white p-8 md:p-12 shadow-lg rounded-xl text-center w-[90%] max-w-md">
-    <h1 class="text-4xl font-semibold text-blue-700 mb-4">Welcome to <span class="text-gray-900">AdminPro</span></h1>
+    <h1 class="text-4xl font-semibold text-transparent bg-clip-text bg-gradient-to-tr from-blue-600 to-blue-400 mb-4">Welcome to <span class="text-gray-900">AdminPro</span></h1>
     <p class="text-gray-600 text-base mb-8">Your personalized admin dashboard</p>
-      <button class="w-full bg-blue-600 text-white py-3 px-6 rounded-lg font-medium text-sm tracking-wide hover:bg-blue-700 transition duration-300 cursor-pointer" id="get-started">
+      <button class="w-full bg-gradient-to-tr from-blue-600 to-blue-400 text-white py-3 px-6 rounded-lg font-medium text-sm tracking-wide hover:shadow-lg hover:from-blue-700 transition duration-300 cursor-pointer" id="get-started">
         Get Started
       </button>
   </div>
